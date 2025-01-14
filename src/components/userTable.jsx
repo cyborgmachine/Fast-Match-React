93a659f4d0510d57d05eb563e977e719af2264fc
cybrogmachine <disastercyborg@gmail.com>
2025-01-15 00:04:57 +0100

    Header Count Function Added

--- src/components/userTable.jsx
+++ src/components/userTable.jsx
@@ -1,66 +1,104 @@
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import React, { useState, useEffect } from 'react';
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from "react";
 // import Users from './users';
-import API from '../api/index.js';
+import API from "../api/index.js";
 
-function UserTable ()  {
+function UserTable() {
+  const [users, setUsers] = useState([]);
 
-const [users,setUsers] =  useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const getUsers = await API.users.fetchAll();
+      setUsers(getUsers);
+    };
+    fetchUsers();
+  }, []);
 
-useEffect(() => {
-const fetchUsers = async () => {
-    const getUsers = await API.users.fetchAll()
-    setUsers(getUsers)
-}
-fetchUsers()
-},[])
+  // Visible Table
+  const isTableVisible = users.length > 0;
 
-  // Функция удаления пользователя
+  // Delete User Function
   const remove = (userToRemove) => {
     setUsers(users.filter((user) => user._id !== userToRemove._id));
   };
 
+  // Header User Count Function
+  function userQuantity() {
+    if (users.length > 1) {
+      return (
+        <h2 id="header-total">
+          <span className="badge bg-primary">
+            {users.length} People hanging out with you today
+          </span>
+        </h2>
+      );
+    } else if (users.length === 1) {
+      return (
+        <h2 id="header-total">
+          <span className="badge bg-primary">
+            {users.length} Person hanging out with you today
+          </span>
+        </h2>
+      );
+    } else {
+      return (
+        <h2 id="header-total">
+          <span className="badge bg-danger">
+            Nobody hanging out with you today
+          </span>
+        </h2>
+      );
+    }
+  }
 
-    const listUsers = users.map((user) => (
-      
-        <tr id='userLine' key={user._id} >
+  const listUsers = users.map((user) => (
+    <tr id="userLine" key={user._id}>
+      <td>{user.name}</td>
 
-    <td>{user.name}</td>
+      <td className="td-display">
+        {user.qualities.map((quality) => (
+          <span
+            className={"td-text badge rounded-pill text-bg-" + quality.color}
+          >
+            {quality.name}
+          </span>
+        ))}
+      </td>
+      <td>{user.profession.name}</td>
+      <td>{user.completedMeetings}</td>
+      <td>{user.rate} /5</td>
 
-
-    <td className='td-display'>
-      {user.qualities.map(quality => <span className={'td-text badge rounded-pill text-bg-' + quality.color}>{quality.name}</span> )} 
-         </td> 
-    <td>{user.profession.name}</td>
-    <td>{user.completedMeetings}</td>
-    <td>{user.rate} /5</td>
-    
-    {/* Delete Button */}
-    <td><Button variant="danger" onClick={() => remove(user)}>Delete</Button></td>
-    
+      {/* Delete Button */}
+      <td>
+        <Button variant="danger" onClick={() => remove(user)}>
+          Delete
+        </Button>
+      </td>
     </tr>
-    
-    ));
-    return (<>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Qualities</th>
-              <th>Proffesion</th>
-              <th>Met (times)</th>
-              <th>Score</th>
-            </tr>
-          </thead>
-          <tbody>
-             {listUsers}
-             
-          </tbody>
-        </Table>
-        </>
-    ) 
+  ));
+  return (
+    <>
+      {userQuantity()}
+      <Table
+        striped
+        bordered
+        hover
+        style={{ visibility: !isTableVisible ? "hidden" : "visible" }}
+      >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Qualities</th>
+            <th>Proffesion</th>
+            <th>Met (times)</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>{listUsers}</tbody>
+      </Table>
+    </>
+  );
 }
-
 
 export default UserTable;
